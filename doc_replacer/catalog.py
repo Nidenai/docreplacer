239a996d5381e610cc753434c901d3d10ea5c500
Dmitriy Knyazev <dimsknz@gmail.com>
2021-02-09 12:03:39 +0700

    Update catalog.py

--- doc_replacer/catalog.py
+++ doc_replacer/catalog.py
@@ -3,10 +3,14 @@ import os
 # создаем мультиплатформенность и пишем создание каталога в разных операционных системах
 
 def create_catalog():
-	homedir = os.path.expanduser("~")
-	homedir_full = homedir + '/Documents'
-	if not os.path.isdir == homedir + '/Документы/Doc Replacer':
-		os.mkdir(homedir + '/Документы/Doc Replacer')
-		
-create_catalog()
+    homedir = os.path.expanduser("~")
+    name_os = os.name
+        
+    if name_os == 'posix':
+                if not os.path.isdir == homedir + '/Документы/Doc Replacer':
+                        os.mkdir(homedir + '/Документы/Doc Replacer')
+    elif name_os == 'nt':
+        if not os.path.isdir == homedir + '\OneDrive\Документы\Doc Replacer':
+            os.mkdir(homedir + '\OneDrive\Документы\Doc Replacer')
+
 	
