3d80e1681ae890c8f0f939fbae7400a5c2e1cdb7
Dmitriy Knyazev <dimsknz@gmail.com>
2021-02-09 21:22:57 +0700

    fix: теперь на юникс точно папка либо создается, либо, если она есть, функция просто ничего не делает

--- doc_replacer/catalog.py
+++ doc_replacer/catalog.py
@@ -8,10 +8,10 @@ def create_catalog():
     name_os = os.name
 
     if name_os == 'posix':
-        if not os.path.isdir == homedir + '/Документы/Doc Replacer':
+        if not os.path.isdir(homedir + '/Документы/Doc Replacer'):
             os.mkdir(homedir + '/Документы/Doc Replacer')
     elif name_os == 'nt':
-        if not os.path.isdir == homedir + '\OneDrive\Документы\Doc Replacer':
+        if not os.path.isdir(homedir + '\OneDrive\Документы\Doc Replacer'):
             os.mkdir(homedir + '\OneDrive\Документы\Doc Replacer')
 
 create_catalog()
