9ac6c84ec89a984aeec0cec80ebdcdeed9090a91
Dmitriy Knyazev <dimsknz@gmail.com>
2021-02-09 19:10:48 +0700

    промежуточный фикс

--- doc_replacer/catalog.py
+++ doc_replacer/catalog.py
@@ -1,16 +1,17 @@
 import os
 
+
 # создаем мультиплатформенность и пишем создание каталога в разных операционных системах
 
 def create_catalog():
     homedir = os.path.expanduser("~")
     name_os = os.name
-        
+
     if name_os == 'posix':
-                if not os.path.isdir == homedir + '/Документы/Doc Replacer':
-                        os.mkdir(homedir + '/Документы/Doc Replacer')
+        if not os.path.isdir == homedir + '/Документы/Doc Replacer':
+            os.mkdir(homedir + '/Документы/Doc Replacer')
     elif name_os == 'nt':
         if not os.path.isdir == homedir + '\OneDrive\Документы\Doc Replacer':
             os.mkdir(homedir + '\OneDrive\Документы\Doc Replacer')
 
-	
+create_catalog()
