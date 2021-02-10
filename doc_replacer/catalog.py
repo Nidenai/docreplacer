e7aca031db07b042e4addfd326dcdb46415e21f5
Dmitriy Knyazev <dimsknz@gmail.com>
2021-02-10 18:02:06 +0700

    Update catalog.py
    feat: add function for checking working catalog

--- doc_replacer/catalog.py
+++ doc_replacer/catalog.py
@@ -14,4 +14,19 @@ def create_catalog():
         if not os.path.isdir(homedir + '\OneDrive\Документы\Doc Replacer'):
             os.mkdir(homedir + '\OneDrive\Документы\Doc Replacer')
 
-create_catalog()
+# создаем переменную, в которую записываем наименование текущего каталога, в зависимости от оси
+name_os = os.name
+homedir = os.path.expanduser("~")
+working_catalog = ''
+if name_os == 'posix':
+    working_catalog = (homedir + '/Документы/Doc Replacer')
+elif name_os == 'nt':
+    working_catalog = (homedir + '\OneDrive\Документы\Doc Replacer')
+
+# записываем в переменную весь список того, что есть в каталоге:
+
+string_of_dir = str(os.listdir(working_catalog))
+
+
+
+
